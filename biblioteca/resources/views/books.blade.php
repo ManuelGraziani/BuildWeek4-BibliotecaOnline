a7b545fcf4473186de3c00a7f31a1ab3de42f94f
Giulius94 <giuliuscb@protonmail.com>
2024-03-21 17:39:08 +0100

    adding layout

--- biblioteca/resources/views/books.blade.php
+++ biblioteca/resources/views/books.blade.php
@@ -1,58 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+    @extends('layouts.layout')
+    @section('title', 'Books Library')
+    @section('content')
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+    @foreach($books as $key => $value)
+        <div class="col">
+            <div class="card">
+                <img src="https://www.lumien.it/wp-content/uploads/2022/01/Come-promuovere-un-libro.jpg" class="card-img-top" alt="..." >
+                <div class="card-body">
+                    <h5 class="card-title">Nome Libro :{{$value->title}}</h5>
+                    <p class="card-text">ID libro :{{$value->id}}</p>
+                    <p class="card-text">Pagine :{{$value->pages}}</p>
+                    <p class="card-text">Anno pubblicazione : {{$value->year}}</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Library</h1>
-
-
-    <div class="col">
-        @foreach($books as $key => $value)
-        <div class="card">
-            <img src="https://www.lumien.it/wp-content/uploads/2022/01/Come-promuovere-un-libro.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Nome Libro :{{$value->title}}</h5>
-                <p class="card-text">ID libro :{{$value->id}}</p>
-                <p class="card-text">Pagine :{{$value->pages}}</p>
-                <p class="card-text">Anno pubblicazione : {{$value->year}}</p>
-                </p>
-
-                @foreach ($value->authors as $user)
-                <p class="card-text">Autore : {{$user->name}}</p>
-
-                @endforeach
-
-                @foreach ($value->categories as $categoriesItem)
-                <p class="card-text">Categoria: {{$categoriesItem->name}}</p>
-
-                @endforeach
-
-                @foreach ($value->reservations as $reservations)
-                <p class="card-text">Prenotazioni : {{$reservations->stato}}</p>
-                <p class="card-text">ID Utente Prenotazione : {{$reservations->user_id}}</p>
-                @endforeach
-
-                @foreach($value->reservations as $reservation)
-                    @foreach($users as $user)
-                        @if($reservation->user_id === $user->id)
-                        <p class="card-text">Nome Utente: {{$user->name}}</p>
-                        @endif
+                    @foreach ($value->authors as $user)
+                    <p class="card-text">Autore : {{$user->name}}</p>
                     @endforeach
-                @endforeach
 
+                    @foreach ($value->categories as $categoriesItem)
+                    <p class="card-text">Categoria: {{$categoriesItem->name}}</p>
+                    @endforeach
+
+                    @foreach ($value->reservations as $reservations)
+                    <p class="card-text">Prenotazioni : {{$reservations->stato}}</p>
+                    <p class="card-text">ID Utente Prenotazione : {{$reservations->user_id}}</p>
+                    @endforeach
+
+                    @foreach($value->reservations as $reservation)
+                        @foreach($users as $user)
+                            @if($reservation->user_id === $user->id)
+                            <p class="card-text">Nome Utente: {{$user->name}}</p>
+                            @endif
+                        @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
-
-
-
-
-</body>
-
-</html>
+    @endsection
