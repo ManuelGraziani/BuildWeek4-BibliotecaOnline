6d5a19f6ec95d85d81151d3fc2d4020f7cb416e9
0znerol <lorenzo.pezzica98@gmail.com>
2024-03-22 11:41:55 +0100

    working author or title search

--- biblioteca/resources/views/layouts/layout.blade.php
+++ biblioteca/resources/views/layouts/layout.blade.php
@@ -14,5 +14,34 @@
            @yield('content')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+
+      const search = document.getElementById('search');
+      const cards_titles = document.querySelectorAll('.card-title');
+      const card_authors = document.querySelectorAll('#card-author');
+
+      search.addEventListener('input', (e) => {
+          const value = e.target.value.toLowerCase();
+
+          cards_titles.forEach(card_title => {
+              const card = card_title.closest('.card');
+              const cardTitleMatch = card_title.textContent.toLowerCase().includes(value);
+              let authorMatch = false;
+
+              const authors = card.querySelectorAll('#card-author');
+              authors.forEach(author => {
+                  if (author.textContent.toLowerCase().includes(value)) {
+                      authorMatch = true;
+                  }
+              });
+
+              if (cardTitleMatch || authorMatch) {
+                  card.style.display = 'block';
+              } else {
+                  card.style.display = 'none';
+              }
+          });
+      });
+    </script>
   </body>
 </html>
